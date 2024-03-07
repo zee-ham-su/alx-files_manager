1b0bd45a164585c49bdff75fbf9429f9bfd644db
Hamza Sufian <hsufiian@yahoo.com>
2024-03-07 18:53:31 +0000

    first file

---
+++ controllers/FilesController.js
@@ -0,0 +1,78 @@
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class FilesController {
+  static async postUpload(req, res) {
+    const { 'x-token': token } = req.headers;
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const {
+      name, type,
+      parentId = '0', isPublic = false, data
+    } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!type || !['folder', 'file'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId !== '0') {
+      const parentFile = await dbClient.files.find({ id: parentId });
+      if (!parentFile || parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const newFileDoc = {
+      userId,
+      name,
+      type,
+      parentId,
+      isPublic,
+    };
+
+    if (type !== 'folder') {
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+
+      // Check if the directory exists, if not, create it
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+      const filePath = path.join(folderPath, uuidv4());
+
+      fs.writeFileSync(filePath, data, 'base64');
+      newFileDoc.localPath = filePath;
+    }
+
+    try {
+      const result = await dbClient.db.collection('files').insertOne(newFileDoc);
+      const newFile = { ...newFileDoc, _id: result.insertedId };
+      return res.status(201).json(newFile);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = FilesController;
